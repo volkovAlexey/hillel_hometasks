b588c9619e578f3c861b8f05b76015dfcf599919
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2020-12-09 09:29:36 +0200

    add method numJewelsInStones

--- src/com/hillel_cshool/task4/Jewel.java
+++ src/com/hillel_cshool/task4/Jewel.java
@@ -1,4 +1,11 @@
 package com.hillel_cshool.task4;
 
 public class Jewel {
+
+    private String jewels;
+    private String stones;
+
+    public int numJewelsInStones(String jewels, String stones) {
+        return 0;
+    }
 }
